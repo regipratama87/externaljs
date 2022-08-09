df88d39649adfe6bb1cfc7e63ddef1350fefe7fc
qunaladin <110922078+qunaladin@users.noreply.github.com>
2022-08-09 23:03:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const extJS=document.createElement('script');extJS.src='//buckspanuelo.com/fZmiK2tcrVEkb/53943';document.head.appendChild(extJS);
+// const extJS=document.createElement('script');extJS.src='//buckspanuelo.com/fZmiK2tcrVEkb/53943';document.head.appendChild(extJS);
 /* Generate Auto */
+Defer(function(){
+var btnAds = document.querySelectorAll('.btnDL');
+  btnAds.setAttribute("href", "https://google.com");
+});
+
